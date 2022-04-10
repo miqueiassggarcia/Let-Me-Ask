3fe6ba3d8cf2f8b627821b5b4c3fde127376f134
Miquéias Garcia <mglucenalucena@gmail.com>
2022-04-09 21:28:48 -0300

    Criando fluxo de login com firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import { Home } from "./pages/Home";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import { Home } from './pages/Home';
+import { NewRoom } from "./pages/NewRoom";
+
+import { AuthContextProvider } from "./contexts/AuthContext";
 
 function App() {
   return (
-    <Home /> 
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/rooms/new" element={<NewRoom />} />
+        </Routes>
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
