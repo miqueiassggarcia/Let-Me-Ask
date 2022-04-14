3b43189647e4bb55daa0acfda1cc16528302edfe
Miquéias Garcia <mglucenalucena@gmail.com>
2022-04-13 22:05:33 -0300

    Página de administrador criada

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,10 +3,15 @@ import { ButtonHTMLAttributes } from "react";
 import "../styles/button.scss";
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement
->;
+> & {
+  isOutLined?: boolean;
+};
 
-export function Button(props: ButtonProps) {
+export function Button({ isOutLined = false, ...props }: ButtonProps) {
   return (
-    <button className='button' {...props} />
+    <button
+      className={`button ${isOutLined ? 'outlined' : ''}`}
+      {...props}
+    />
   )
 }
